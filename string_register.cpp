58948b38235b84d4371d9e9fba92c84913b0cfe0
ValeriaSamsonova <samsonova147@gmail.com>
2017-03-18 12:55:27 +0500

    Практика 3, задание 1

---
+++ string_register.cpp
@@ -0,0 +1,85 @@
+#include "stdafx.h"
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+
+
+void openFiles(int argc, char * argv[], FILE **input, FILE **output);
+void closeFiles(int argc, char * argv[], FILE **input, FILE **output);
+void readString(char *A, FILE *input);
+
+
+int main(int argc, char *argv[]) {
+	FILE *input, *output;
+	char A[100];
+	input = NULL;
+	output = NULL;
+
+	openFiles(argc, argv, &input, &output);
+	readString(A, input);
+
+	for (int i = 0; i < strlen(A); i++)
+	{
+		if (isalpha(A[i]))
+		{
+			if (islower(A[i]))
+				A[i] = toupper(A[i]);
+			else
+				A[i] = tolower(A[i]);
+		}
+	}
+
+	fprintf(output,"A: %s \n", A);
+	closeFiles(argc, argv, &input, &output);
+	getchar();
+	return 0;
+}
+
+void openFiles(int argc, char * argv[], FILE **input, FILE **output)
+{
+	if (argc>1)
+	{
+		for (int i = 1; i<argc; i++)
+		{
+			if (!strcmp(argv[i], "-input") && argc >= i + 2)
+			{
+				if (strcmp(argv[i + 1], "-output"))
+				{
+					*input = fopen(argv[i + 1], "r");
+				}
+			}
+			else if (!strcmp(argv[i], "-output") && argc >= i + 2)
+			{
+				if (strcmp(argv[i + 1], "-input"))
+				{
+					*output = fopen(argv[i + 1], "w");
+				}
+			}
+		}
+	}
+}
+
+void closeFiles(int argc, char * argv[], FILE **input, FILE **output)
+{
+	if (*input != NULL)
+	{
+		fclose(*input);
+	}
+	if (*output != NULL)
+	{
+		fclose(*output);
+	}
+}
+
+void readString(char *A, FILE *input)
+{
+	if (input == NULL)
+	{
+		printf("Enter string A : ");
+		fgets(A, 98, stdin);
+	}
+	else
+	{
+		fgets(A, 98, input);
+	}
+}
